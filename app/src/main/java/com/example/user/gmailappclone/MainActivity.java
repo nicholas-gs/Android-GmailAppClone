c508eef4e99779970505a1ffc306e821676e3389
Nicholas <nicholasganshyan@gmail.com>
2018-12-26 22:04:34 +0800

    Add Navigation Drawer

--- app/src/main/java/com/example/user/gmailappclone/MainActivity.java
+++ app/src/main/java/com/example/user/gmailappclone/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.user.gmailappclone;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private FrameLayout layoutContainer;
     private NavigationView navigationView;
+
+    private ImageView toggleArrow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,20 +28,76 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initializeWidgets();
+
+        toggleNavigationDrawer();
+        handleDrawerMenuToggle();
         setDefaultFragment();
     }
 
-    private void initializeWidgets(){
+    private void initializeWidgets() {
         drawerLayout = findViewById(R.id.main_drawerlayout);
         layoutContainer = findViewById(R.id.main_layout_container);
-        navigationView = findViewById(R.id.main_navigationview);
-        toolbar = findViewById(R.id.main_toolbar);
 
+        toolbar = findViewById(R.id.main_toolbar);
         toolbar.setTitle("Primary");
+        setSupportActionBar(toolbar);
+
+        navigationView = findViewById(R.id.main_navigationview);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
-    private void setDefaultFragment(){
-        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout_container,new PrimaryFragment())
-                .commit();
+    private void setDefaultFragment() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout_container,
+                new PrimaryFragment()).commit();
+        navigationView.setCheckedItem(R.id.nav_menu_closed_primary);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        if (menuItem.getItemId() == navigationView.getCheckedItem().getItemId()) {
+            // Do nothing so that the fragment is not re-initialised
+        } else {
+            switch (menuItem.getItemId()) {
+                case R.id.nav_menu_closed_primary:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_layout_container, new PrimaryFragment())
+                            .commit();
+                    break;
+                case R.id.nav_menu_closed_social:
+                    Toast.makeText(this, "Social", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.nav_menu_closed_promotions:
+                    Toast.makeText(this, "Promotions", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+        return true;
+    }
+
+    private void toggleNavigationDrawer() {
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.open_drawer, R.string.close_drawer);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+    }
+
+    private void handleDrawerMenuToggle() {
+        View header = navigationView.getHeaderView(0);
+        toggleArrow = header.findViewById(R.id.nav_header_selector);
+
+        toggleArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v.isSelected()) {
+                    v.setSelected(false);
+                    navigationView.getMenu().clear();
+                    navigationView.inflateMenu(R.menu.nav_menu_closed);
+
+                } else {
+                    v.setSelected(true);
+                    navigationView.getMenu().clear();
+                    navigationView.inflateMenu(R.menu.nav_menu_open);
+                }
+            }
+        });
     }
 }
